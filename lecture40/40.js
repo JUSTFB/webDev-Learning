c28c8399c4da7eaf39231784cac9afa0b8448383
fenil <fpvijapur123@gmail.com>
2025-10-06 10:56:12 +0530

    adding webdev learning course till css

---
+++ lecture40/40.js
@@ -0,0 +1,23 @@
+// var age =25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+
+function solve (){
+    var age = 45;
+   
+   
+}
+ 
+solve();
+{var b=30;}
+console.log(b);
+{
+let a=10;
+console.log(a);
+}
+
+var let=40;
+// console.log(let); dont use this type of namingṇṇṇṇṇ
---
+++ lecture41/41a.js
@@ -0,0 +1,8 @@
+let a=10;
+let b=5;
+console.log(++a);
+
+let age =10;
+
+let can = (age<18 ) ? "i am teen" : "i am adult" ;
+console.log(can);
