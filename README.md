1b877596e51a2155505209cba910032b9b8a0d8d
fenil <fpvijapur123@gmail.com>
2025-12-28 20:26:18 +0530

    lecture41

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 #this is my web dev repo
-it is my learnings ot projects but i done some projects 
+it is my learnings and projects but i done some projects 
 you can see it in lecture n 33-38
--- lecture41/41a.js
+++ lecture41/41a.js
@@ -6,3 +6,21 @@ let age =10;
 
 let can = (age<18 ) ? "i am teen" : "i am adult" ;
 console.log(can);
+
+let markr =35;
+let result =(markr>33)? "pass" : "fail";
+console.log(result);
+
+console.log(false || 6);
+console.log(true && 0);
+console.log("grape" && 5);
+console.log(true || 0 ||5||7);
+console.log(2|6);
+console.log(2&6);
+console.log(2&5);
+console.log(~0);
+console.log(3^6);
+console.log(~2);
+console.log(10>>3); 
+
+
