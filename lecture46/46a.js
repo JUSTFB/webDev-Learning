caa130e23d66763c0a6b6a691b94466878c762a8
fenil <fpvijapur123@gmail.com>
2026-01-06 16:02:17 +0530

    46 updated

---
+++ lecture46/46a.js
@@ -0,0 +1,42 @@
+// // variable scoping in function
+
+
+// // global scope,we can use this variable any whare in the code
+// var age =15;
+// // let annd const will work for this scenario
+
+// for(let i=0;i<5;i++){
+//     console.log(age);
+// }
+
+
+// function func1(){
+//     var name="fenil";
+//     console.log(name);
+//     // var function scope 
+// }
+// // cant use them outside function 
+// console.log(name);
+
+// // also fr let and const
+
+
+// // block scope
+
+// // var is not block scope
+
+// {
+
+//     var height =175;
+// }
+// console.log(height); // will work
+
+// {
+//     let weight =58;
+// }   
+// console.log(weight); // will not work
+
+// // TEMPORAL DEAD ZONE
+
+// // ASK CHATGPT
+
